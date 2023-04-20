a3a41b081a9af6559d606f17d85616efa1e34ddb
Raghav Singh <raghvender3@gmail.com>
2023-04-20 20:10:26 +0530

    new

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/raghavsingh/projects/pinbank/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\",\"display\":\"swap\"}],\"variableName\":\"inter\"}": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
